6557bc063168e9ce1650ac7eef34c4659063f2ba
maglib420 <libergmagnus@gmail.com>
2022-09-12 14:17:34 +0200

    first commit

---
+++ Hoved.py
@@ -0,0 +1,31 @@
+from random import randint, random
+
+spillPaaNytt = True
+
+while spillPaaNytt == True:
+    forsøk = 1
+    nummer = randint(1,10)
+
+    gjett = int(input("Gjett et tall mellom 1 og 10."))
+
+    while gjett != nummer:
+        if gjett < nummer:
+            print("Du gjettet for lavt.")
+            gjett = int(input("Gjett igjen."))
+            forsøk = forsøk + 1
+        elif gjett > nummer:
+            print("Du gjettet for høyt.")
+            gjett = int(input("Gjett igjen."))
+            forsøk = forsøk + 1
+
+    print()
+    print("Du gjettet riktig nummer!")
+    print("Det tok deg bare", forsøk, "forsøk :)")
+    print()
+    spillPaaNyttSvar = input("Vil du spille igjen? Svar 'Ja eller Nei")
+
+    print(spillPaaNytt)
+
+    if spillPaaNyttSvar == "Nei":
+        spillPaaNytt = False
+
